7cf5874a51b92fa5e35eccd68f28c139768159b0
miaschwarz <schwarz.mia@gmail.com>
2025-04-09 10:28:40 +0100

    set up routing, new register page & test of register page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import Register from './Register.jsx'; // make sure this is the correct path and file name
+import './App.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  </BrowserRouter>
+);
---
+++ src/register.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+
+
+function Register() {
+    return (
+      <div className="app-container">
+        <div className="quiz-box">
+          <h2>🐾 Register New User</h2>
+          {/* form fields */}
+        </div>
+      </div>
+    );
+  }
+    
+export default Register;
