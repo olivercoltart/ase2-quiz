e9594b08cf2be91872517dc8665fe8e3365b7018
olivercoltart <144618905+olivercoltart@users.noreply.github.com>
2025-04-15 16:39:04 +0100

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true, // Allows using `expect`, `describe`, `it`, etc. globally
+    environment: 'jsdom', // Use jsdom for DOM testing (or 'node' if you're testing in Node.js environment)
+  },
+});
